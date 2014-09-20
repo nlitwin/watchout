06907cdfd8a990191eceedea4beea4251b858ca4
polynickglot <nicklitwin88@gmail.com>
2014-09-19 20:38:12 -0700

    Implement collisions

--- watchout.js
+++ watchout.js
@@ -1,19 +1,17 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies: 30,
+  nEnemies: 10,
   padding: 20
 };
 
 var gameStats = {
   score: 0,
-  bestScore: 0
+  bestScore: 0,
+  collisions: 0,
+  collisionsThisRound: 0
 };
 
-var axes = {
-  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
-  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
-};
 
 var gameBoard = d3.select('.gameBoard').append('svg:svg')
                   .attr('width', gameOptions.width)
@@ -27,8 +25,7 @@ function shuffle() {
   for (var i = 0; i < gameOptions.nEnemies; i++) {
     var o = {
       'cx': Math.random() * gameOptions.width,
-      'cy': Math.random() * gameOptions.height,
-      'r': Math.random() * 20
+      'cy': Math.random() * gameOptions.height
     };
     enemyPositions[i] = o;
   }
@@ -49,29 +46,69 @@ var enemies = gameBoard.selectAll('circle')
   .attr('cy', function(d){
     return d.cy;
   })
-  .attr('r', function(d){
-    return d.r;
-  });
+  .attr('r', '10');
 
 function update(){
   enemies.data(enemyPositions)
-    .transition().duration(1000)
-    .attr('cx', function(d){
-      return d.cx;
-    })
-    .attr('cy', function(d){
-      return d.cy;
-    })
-    //.attr('fill', 'black')
-    .attr('r', function(d){
-      return d.r;
-    });
+    .transition().duration(2000)
+    .tween('custom', tweenWithCollisionDetection);
+}
+
+function tweenWithCollisionDetection(endData){
+  var enemy = d3.select(this);
+  var startPos = {
+    x: parseFloat(enemy.attr('cx')),
+    y: parseFloat(enemy.attr('cy'))
+  }
+  var endPos = {
+    x: endData.cx,
+    y: endData.cy
+  }
+  return function(t){
+    checkCollision(enemy, onCollision);
+    var enemyNextPos = {
+      x: startPos.x + (endPos.x - startPos.x) * t,
+      y: startPos.y + (endPos.y - startPos.y) * t
+    };
+    enemy.attr('cx', enemyNextPos.x)
+        .attr('cy', enemyNextPos.y);
+    gameStats.score += 0.01;
+    d3.select('.current span').data([gameStats.score.toFixed(0)])
+      .text(function(d){
+        return d;
+      });
+  };
+}
+
+function checkCollision(enemy, onCollision){
+  var radiusSum =  parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
+  var xDiff = parseFloat(enemy.attr('cx')) - parseFloat(player.attr('cx'));
+  var yDiff = parseFloat(enemy.attr('cy')) - parseFloat(player.attr('cy'));
+  var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) );
+  if (separation < radiusSum){
+    // Collision
+    //
+    onCollision();
+  }
+}
+
+function onCollision(){
+  gameStats.collisions++;
+  if (gameStats.score > gameStats.bestScore) {
+    gameStats.bestScore = gameStats.score.toFixed(0);
+  }
+  gameStats.score = 0;
+  d3.selectAll('span').data([gameStats.bestScore,
+    gameStats.score, gameStats.collisionsThisRound + gameStats.collisions])
+      .text(function(d) {
+        return d;
+      });
 }
 
 setInterval(function(){
   shuffle();
   update();
-}, 1000);
+}, 2000);
 
 var player = gameBoard.selectAll('circle.player')
   .data([{
@@ -106,7 +143,9 @@ var drag = d3.behavior.drag().on('drag', function(){
   }
   player.attr('cx', newX);
   player.attr('cy', newY);
+
 });
 d3.selectAll(".player").call(drag);
+
 
 
